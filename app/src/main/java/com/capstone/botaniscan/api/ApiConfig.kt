8059c614c90a418933dfb342b9721efc1e0a58db
nursalim92 <56105983+nursalim92@users.noreply.github.com>
2023-06-17 00:10:44 +0700

    Create ApiConfig.kt

---
+++ app/src/main/java/com/capstone/botaniscan/api/ApiConfig.kt
@@ -0,0 +1,27 @@
+package com.capstone.botaniscan.api
+
+
+import com.capstone.botaniscan.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiConfig {
+
+    fun getApiService(): ApiService {
+        val loggingInterceptor =
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BuildConfig.API_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+        return retrofit.create(ApiService::class.java)
+    }
+
+}
